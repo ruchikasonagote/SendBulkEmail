4b1dbf94c229c6202d1bf744e040c4911835fb0d
ruchikasonagote <111609654+ruchikasonagote@users.noreply.github.com>
2024-04-05 17:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = 'divyamadineni@iitgn.ac.in'
-app.config['MAIL_PASSWORD'] = 'njkwpjvnodqrbbkn'
+app.config['MAIL_PASSWORD'] = '*********'
 app.config['MAIL_USE_SSL'] = True
 
 from flask_mail import Mail, Message

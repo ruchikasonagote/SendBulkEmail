0593800251144d3fcf1e6e829ab810b0421182d0
ruchikasonagote <111609654+ruchikasonagote@users.noreply.github.com>
2024-04-12 04:06:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'  # Change this to your MySQL username
-MYSQL_PASSWORD = '@RU96chi37KA@'  # Change this to your MySQL password
+MYSQL_PASSWORD = '*****'  # Change this to your MySQL password
 MYSQL_DB = 'BULKEMAIL_updated'
